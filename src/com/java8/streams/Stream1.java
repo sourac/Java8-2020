cd23e8995c93ea51cac6dd15e15c43f444ecc228
Atul Sharma <atulsharma21772@gmail.com>
2020-06-09 01:49:10 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/java8/streams/Stream1.java
+++ src/com/java8/streams/Stream1.java
@@ -7,8 +7,13 @@ public class Stream1 {
 	public static void main(String[] args) {
 
 		//way to create a stream
+		//this is a stream of val
 		Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 		stream.forEach(p->System.out.println(p));
+		
+		//stream of array of elements
+		Stream<Integer> stream1=Stream.of(new Integer[] {1,2,3,4,5,6,7,8,9});
+		stream1.forEach(p->System.out.println(p));
 
 	}
 
