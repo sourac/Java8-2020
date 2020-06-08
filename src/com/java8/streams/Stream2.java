afc4589327dc3626e1a732b50c1f2e107cab5f1c
Atul Sharma <atulsharma21772@gmail.com>
2020-06-09 02:07:41 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

---
+++ src/com/java8/streams/Stream2.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.java8.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Atul Sharma
+ *
+ *         https://github.com/sourac
+ */
+public class Stream2 {
+
+	public static void main(String[] args) {
+		List<String> stringCollection = new ArrayList<>();
+		stringCollection.add("ddd2");
+		stringCollection.add("aaa2");
+		stringCollection.add("bbb1");
+		stringCollection.add("aaa1");
+		stringCollection.add("bbb3");
+		stringCollection.add("ccc");
+		stringCollection.add("bbb2");
+		stringCollection.add("ddd1");
+		
+		
+		
+	}
+
+}
