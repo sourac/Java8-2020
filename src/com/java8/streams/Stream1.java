138f05b3e771bcead74d0e078c46481c2882157f
Atul Sharma <atulsharma21772@gmail.com>
2020-06-09 01:42:37 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/java8/streams/Stream1.java
+++ src/com/java8/streams/Stream1.java
@@ -1,5 +1,15 @@
 package com.java8.streams;
 
+import java.util.stream.Stream;
+
 public class Stream1 {
+
+	public static void main(String[] args) {
+
+		//way to create a stream
+		Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		stream.forEach(p->System.out.println(p));
+
+	}
 
 }
