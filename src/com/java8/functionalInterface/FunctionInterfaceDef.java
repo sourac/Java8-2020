12e9c45ff5e13322e8383d62108ca79dc0ee669e
Atul Sharma <atulsharma21772@gmail.com>
2020-06-11 00:04:26 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/java8/functionalInterface/FunctionInterfaceDef.java
+++ src/com/java8/functionalInterface/FunctionInterfaceDef.java
@@ -6,8 +6,12 @@ package com.java8.functionalInterface;
 /**
  * @author Atul Sharma
  *
- * https://github.com/sourac
+ *         https://github.com/sourac
  */
+
+@FunctionalInterface
 public interface FunctionInterfaceDef {
 
+	public void doSomething();
+	//public void doSomethingMore();
 }
