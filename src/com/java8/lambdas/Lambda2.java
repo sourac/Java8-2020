e9d473fafd95481e3b6474cf70dbedfe09e32e73
Atul Sharma <atulsharma21772@gmail.com>
2020-06-11 00:15:24 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/java8/lambdas/Lambda2.java
+++ src/com/java8/lambdas/Lambda2.java
@@ -6,18 +6,30 @@ package com.java8.lambdas;
 /**
  * @author Atul Sharma
  *
- * https://github.com/sourac
+ *         https://github.com/sourac
  */
+
+interface something {
+	public void doShit();
+}
+
 public class Lambda2 {
-	
+
 	/*
-	 * Lambda function are function without name.
-	 * they're used to provide the implementation of the functional interface.
+	 * Lambda function are function without name. they're used to provide the
+	 * implementation of the functional interface.
 	 * 
-	 * */
-	
+	 */
+
 	public static void main(String[] args) {
 		
+		int shit=10;
+		something someShit= ()->{
+			System.out.println("Doing shit from last " + shit + " minutes");
+		};
+		
+		someShit.doShit();
+
 	}
 
 }
