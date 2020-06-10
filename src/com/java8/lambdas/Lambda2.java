21263e85b4d799493f64d5a5f708364ac88b98ed
Atul Sharma <atulsharma21772@gmail.com>
2020-06-11 00:01:35 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

---
+++ src/com/java8/lambdas/Lambda2.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.java8.lambdas;
+
+/**
+ * @author Atul Sharma
+ *
+ * https://github.com/sourac
+ */
+public class Lambda2 {
+	
+	/*
+	 * Lambda function are function without name.
+	 * they're used to provide the implementation of the functional interface.
+	 * 
+	 * */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
