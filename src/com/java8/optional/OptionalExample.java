468089030f2c3f270f89d73c2b1348680bcfd688
Atul Sharma <atulsharma21772@gmail.com>
2020-06-16 00:30:28 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/java8/optional/OptionalExample.java
+++ src/com/java8/optional/OptionalExample.java
@@ -9,7 +9,10 @@ package com.java8.optional;
  * https://github.com/sourac
  */
 public class OptionalExample {
-	
+	/*
+	 * introduced in java-8, used to deal with NullPointerException in java.
+	 * it's in the util package.
+	 * */
 	public static void main(String[] args) {
 		
 	}
