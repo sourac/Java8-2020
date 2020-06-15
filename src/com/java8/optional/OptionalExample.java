0ee7d02c08ef0102be96e2422a7d3dc9fa3569aa
Atul Sharma <atulsharma21772@gmail.com>
2020-06-16 00:28:03 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

---
+++ src/com/java8/optional/OptionalExample.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.java8.optional;
+
+/**
+ * @author Atul Sharma
+ *
+ * https://github.com/sourac
+ */
+public class OptionalExample {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
