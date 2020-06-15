2eecb543249a9f458c20a89822ad0e0c1098a10c
Atul Sharma <atulsharma21772@gmail.com>
2020-06-16 00:34:22 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/java8/optional/OptionalExample.java
+++ src/com/java8/optional/OptionalExample.java
@@ -3,18 +3,29 @@
  */
 package com.java8.optional;
 
+import java.util.Optional;
+
 /**
  * @author Atul Sharma
  *
- * https://github.com/sourac
+ *         https://github.com/sourac
  */
 public class OptionalExample {
 	/*
-	 * introduced in java-8, used to deal with NullPointerException in java.
-	 * it's in the util package.
-	 * */
+	 * introduced in java-8, used to deal with NullPointerException in java. it's in
+	 * the util package.
+	 */
 	public static void main(String[] args) {
+
+		String [] str=new String[10];
+		Optional<String> opt = Optional.ofNullable(str[1]);
+		if(opt.isPresent()) {
+			System.out.println("value is present...");
+		}else {
+			System.out.println("value is not present...");
+		}
 		
+
 	}
 
 }
