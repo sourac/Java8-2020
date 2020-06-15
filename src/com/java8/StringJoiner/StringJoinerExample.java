36d1d36bb6c723f626201cf26d3a0cfb8df6990a
Atul Sharma <atulsharma21772@gmail.com>
2020-06-15 18:12:06 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/java8/StringJoiner/StringJoinerExample.java
+++ src/com/java8/StringJoiner/StringJoinerExample.java
@@ -3,11 +3,31 @@
  */
 package com.java8.StringJoiner;
 
+import java.util.StringJoiner;
+
 /**
  * @author Atul Sharma
  *
- * https://github.com/sourac
+ *         https://github.com/sourac
  */
-public class StringJoiner {
+public class StringJoinerExample {
+
+	/*
+	 * Stringjoiner class is added in util package and is used to create a sequence
+	 * of character separated by delimiter.
+	 */
+
+	public static void main(String[] args) {
+
+		StringJoiner stringJoiner = new StringJoiner(",");
+		stringJoiner.add("Citrix");
+		stringJoiner.add("Verizone");
+		stringJoiner.add("Amazon");
+		stringJoiner.add("Facebook");
+		stringJoiner.add("Google");
+		
+		System.out.println(stringJoiner);
+
+	}
 
 }
